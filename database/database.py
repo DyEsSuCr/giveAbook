9fecfda90e34ce339f6bfc237d6c105ab8e6596d
DylanSuarez <2000sajo@gmail.com>
2023-10-27 18:18:41 -0500

    change: rename config to env_config

--- database/database.py
+++ database/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-from config.config import DATABASE_URL
+from config.env_config import DATABASE_URL
 
 SQLALCHEMY_DATABASE_URL = DATABASE_URL
 
